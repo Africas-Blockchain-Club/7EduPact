36a1e16b01050f7b6899cdb9931cd76720c59b34
Kat-Devs <katlegophele95@gmail.com>
2024-04-28 00:21:01 +0200

    Smart contracts for student registration, course creation and nft minting for certificate

---
+++ contracts/EduPact_NFT.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./EduPact_StudentContract.sol";
+import "./EduPact_CourseContract.sol";
+
+contract EduPact_NFT is ERC721 {
+    StudentContract private studentContract;
+    CourseContract private courseContract;
+    uint256 private nextTokenId = 0;
+
+    constructor(address _studentContract, address _courseContract) ERC721("EduPactNFT", "EDUNFT") {
+        studentContract = StudentContract(_studentContract);
+        courseContract = CourseContract(_courseContract);
+    }
+
+    function issueCertificate(address studentAddress, string memory courseName) public {
+        require(studentContract.isRegistered(studentAddress), "Student is not registered");
+        require(courseContract.courseExists(courseName), "Course does not exist");
+        require(studentContract.hasCompletedCourse(studentAddress, courseName), "Course is not completed");
+
+        _mint(studentAddress, nextTokenId);
+        nextTokenId++;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    address owner = ownerOf(tokenId); // This will throw if the token does not exist
+
+    (string memory studentName, string memory studentSurname) = studentContract.getStudent(owner);
+    (string memory courseName, string memory courseDescription, uint256 studentCount) = courseContract.getCourse(studentContract.getCourseForStudent(owner));
+
+    string memory json = string(abi.encodePacked('{"name":"', studentName, '","description":"', studentSurname, ' completed ', courseName, '","attributes":[{"trait_type":"Course","value":"', courseName, '"},{"trait_type":"Description","value":"', courseDescription, '"},{"trait_type":"Student Count","value":', studentCount, '}]}'));
+    string memory base64Json = base64encode(bytes(json));
+
+    return string(abi.encodePacked('data:application/json;base64,', base64Json));
+}
+
+    //Function to encode data in base64
+    function base64encode(bytes memory data) private pure returns (string memory) {
+    string memory base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    uint256 encodedLength = 4 * ((data.length + 2) / 3);
+    string memory result = new string(encodedLength + encodedLength / 76);
+
+    bytes memory resultBytes = bytes(result);
+
+    uint256 i = 0;
+    uint256 j = 0;
+    for (; i + 2 < data.length; i += 3) {
+        resultBytes[j++] = bytes(base64Alphabet)[uint256((uint8(data[i]) >> 2) & 0x3F)];
+        resultBytes[j++] = bytes(base64Alphabet)[uint256(((uint8(data[i]) & 0x3) << 4) | ((uint8(data[i + 1]) >> 4) & 0x3F))];
+        resultBytes[j++] = bytes(base64Alphabet)[uint256(((uint8(data[i + 1]) & 0xF) << 2) | ((uint8(data[i + 2]) >> 6) & 0x3F))];
+        resultBytes[j++] = bytes(base64Alphabet)[uint256(uint8(data[i + 2]) & 0x3F)];
+    }
+
+    if (i < data.length) {
+        resultBytes[j++] = bytes(base64Alphabet)[uint256((uint8(data[i]) >> 2) & 0x3F)];
+        if (i + 1 < data.length) {
+            resultBytes[j++] = bytes(base64Alphabet)[uint256(((uint8(data[i]) & 0x3) << 4) | ((uint8(data[i + 1]) >> 4) & 0x3F))];
+            resultBytes[j++] = bytes(base64Alphabet)[uint256((uint8(data[i + 1]) & 0xF) << 2)];
+        } else {
+            resultBytes[j++] = bytes(base64Alphabet)[uint256((uint8(data[i]) & 0x3) << 4)];
+            resultBytes[j++] = '=';
+        }
+        resultBytes[j++] = '=';
+    }
+
+    return string(resultBytes);
+}
+}
+    /* Function to encode data in base64
+    function base64encode(bytes memory data) private pure returns (string memory) {
+    string memory base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    uint256 encodedLength = 4 * ((data.length + 2) / 3);
+    string memory result = new string(encodedLength + encodedLength / 76);
+
+    bytes memory resultBytes = bytes(result);
+
+    uint256 i = 0;
+    uint256 j = 0;
+    for (; i + 2 < data.length; i += 3) {
+        resultBytes[j++] = bytes(base64Alphabet)[uint256((uint8(data[i]) >> 2) & 0x3F)];
+        resultBytes[j++] = bytes(base64Alphabet)[uint256(((uint8(data[i]) & 0x3) << 4) | ((uint8(data[i + 1]) >> 4) & 0x3F))];
+        resultBytes[j++] = bytes(base64Alphabet)[uint256(((uint8(data[i + 1]) & 0xF) << 2) | ((uint8(data[i + 2]) >> 6) & 0x3F))];
+        resultBytes[j++] = bytes(base64Alphabet)[uint256(uint8(data[i + 2]) & 0x3F)];
+    }
+
+    if (i < data.length) {
+        resultBytes[j++] = bytes(base64Alphabet)[uint256((uint8(data[i]) >> 2) & 0x3F)];
+        if (i + 1 < data.length) {
+            resultBytes[j++] = bytes(base64Alphabet)[uint256(((uint8(data[i]) & 0x3) << 4) | ((uint8(data[i + 1]) >> 4) & 0x3F))];
+            resultBytes[j++] = bytes(base64Alphabet)[uint256((uint8(data[i + 1]) & 0xF) << 2)];
+        } else {
+            resultBytes[j++] = bytes(base64Alphabet)[uint256((uint8(data[i]) & 0x3) << 4)];
+            resultBytes[j++] = '=';
+        }
+        resultBytes[j++] = '=';
+    }
+
+    return string(resultBytes);
+}**/
+
---
+++ contracts/EduPact_StudentContract.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./EduPact_CourseContract.sol";
+import "./EduPact_NFT.sol";
+
+// Contract to manage students
+contract StudentContract {
+    // Struct to represent a student
+    struct Student {
+        address studentAddress; // Address of the student
+        string name; // Name of the student
+        string surname; // Surname of the student
+        bool isRegistered; // Flag to check if the student is registered
+        mapping(string => bool) completedCourses; // Mapping from course name to completion status
+    }
+
+    // Instance of the CourseContract
+    CourseContract private courseContract;
+
+    // Instance of the EduPact_NFT contract
+    EduPact_NFT private nftContract;
+    
+
+    // Mapping from student address to Student struct
+    mapping(address => Student) private students;
+
+    // Mapping from student address to course name
+    mapping(address => string) private studentCourses;
+
+    // Constructor to initialize the CourseContract instance
+    constructor(CourseContract _courseContract, EduPact_NFT _nftContract) {
+        courseContract = _courseContract;
+        nftContract = _nftContract;
+
+    }
+
+    
+
+    // Function to register a new student
+    // Requires that the student is not already registered
+    function registerStudent(string memory name, string memory surname) public {
+    require(!students[msg.sender].isRegistered, "User is already registered");
+
+    Student storage student = students[msg.sender];
+    student.studentAddress = msg.sender;
+    student.name = name;
+    student.surname = surname;
+    student.isRegistered = true;
+}
+
+    // Function to register a student for a course
+    // Requires that the student is registered and the course exists
+    function registerForCourse(string memory courseName) public {
+    address studentAddress = msg.sender;
+    require(students[studentAddress].isRegistered, "Student is not registered");
+    require(courseContract.courseExists(courseName), "Course does not exist");
+    studentCourses[studentAddress] = courseName;
+    courseContract.incrementStudentCount(courseName);
+}
+
+    // Function to get the details of a student
+    // Requires that the student is registered
+    function getStudent(address studentAddress) public view returns (string memory, string memory) {
+        require(students[studentAddress].isRegistered, "Student is not registered");
+        return (students[studentAddress].name, students[studentAddress].surname);
+    }
+
+    // Function to get the course for a student
+    // Requires that the student is registered
+    function getCourseForStudent(address studentAddress) public view returns (string memory) {
+        require(students[studentAddress].isRegistered, "Student is not registered");
+        return studentCourses[studentAddress];
+    }
+
+    function completeCourse(string memory courseName) public {
+    address studentAddress = msg.sender;
+    require(students[studentAddress].isRegistered, "Student is not registered");
+    require(!students[studentAddress].completedCourses[courseName], "Course is already completed");
+
+    students[studentAddress].completedCourses[courseName] = true;
+
+    // Call the NFT creation function
+    nftContract.issueCertificate(studentAddress, courseName);
+}
+
+    function isRegistered(address studentAddress) public view returns (bool) {
+        return students[studentAddress].isRegistered;
+    }
+
+    function hasCompletedCourse(address studentAddress, string memory courseName) public view returns (bool) {
+        return students[studentAddress].completedCourses[courseName];
+    }
+
+    
+}
